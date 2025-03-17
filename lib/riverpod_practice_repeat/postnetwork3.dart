2e4bcb7259fdca0998bc8607ce188f1efe1facc1
Vuelta7 <markvuelta07@gmail.com>
2025-03-17 18:46:13 +0800

    im fucking lost

--- lib/riverpod_practice_repeat/postnetwork3.dart
+++ lib/riverpod_practice_repeat/postnetwork3.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -40,11 +42,53 @@ class TodoListNotifier extends _$TodoListNotifier {
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(todo.toJson()),
     );
-
+    print(response.body);
     List<Todo> newTodos = (jsonDecode(response.body) as List)
         .map((data) => Todo.fromJson(data))
         .toList();
 
     state = AsyncData(newTodos);
+  }
+}
+
+void main(List<String> args) {
+  runApp(ProviderScope(child: TodoApp()));
+}
+
+class TodoApp extends ConsumerWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todoListAsync = ref.watch(todoListNotifierProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        body: todoListAsync.when(
+          data: (todos) => ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+              return ListTile(
+                title: Text(todo.description),
+                trailing: todo.completed
+                    ? Icon(Icons.check, color: Colors.green)
+                    : null,
+              );
+            },
+          ),
+          error: (error, stackTrace) => Text('bobo'),
+          loading: () => CircularProgressIndicator(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(todoListNotifierProvider.notifier).addTodo(
+                  Todo(description: 'tangina'),
+                );
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
